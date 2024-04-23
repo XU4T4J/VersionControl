9f37c993fe699ed575d3a262ef312079c2a98579
XU4T4J <daniel.bacsi@stud.uni-corvinus.hu>
2024-04-23 20:48:42 +0200

    3A megcsinálva email teszthez

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
-    internal class AccountControllerTestFixture
+    public class AccountControllerTestFixture
     {
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
+            //Arrange
+            var accountController = new AccountController();
+
+            //Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            //Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+            
 
         }
     }
