d67b8d31ae51a510105e39ae86ccd7457734332a
XU4T4J <daniel.bacsi@stud.uni-corvinus.hu>
2024-04-23 15:21:42 +0200

    resource készítése és labelek beállítása

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBox1 = new ListBox();
+            lblLastName = new Label();
+            lblFirstName = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            btnAdd = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(213, 289);
+            listBox1.TabIndex = 0;
+            // 
+            // lblLastName
+            // 
+            lblLastName.AutoSize = true;
+            lblLastName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lblLastName.Location = new Point(273, 34);
+            lblLastName.Name = "lblLastName";
+            lblLastName.Size = new Size(52, 21);
+            lblLastName.TabIndex = 1;
+            lblLastName.Text = "label1";
+            // 
+            // lblFirstName
+            // 
+            lblFirstName.AutoSize = true;
+            lblFirstName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            lblFirstName.Location = new Point(273, 63);
+            lblFirstName.Name = "lblFirstName";
+            lblFirstName.Size = new Size(52, 21);
+            lblFirstName.TabIndex = 2;
+            lblFirstName.Text = "label2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(359, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(174, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(359, 61);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(174, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAdd.Location = new Point(273, 100);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(260, 35);
+            btnAdd.TabIndex = 5;
+            btnAdd.Text = "button1";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(545, 313);
+            Controls.Add(btnAdd);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(lblFirstName);
+            Controls.Add(lblLastName);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private Label lblLastName;
+        private Label lblFirstName;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button btnAdd;
     }
 }
